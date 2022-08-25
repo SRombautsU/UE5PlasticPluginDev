e5b37e1504277b1b26f4982a6027733fbdd70d4c
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2022-08-25 12:03:24 +0200

    //	bUseUnityBuild = false;
    Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
    WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)

--- Source/UE5PlasticPluginDevEditor.Target.cs
+++ Source/UE5PlasticPluginDevEditor.Target.cs
@@ -10,5 +10,9 @@ public class UE5PlasticPluginDevEditorTarget : TargetRules
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
 		ExtraModuleNames.Add("UE5PlasticPluginDev");
+
+		// Uncomment to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
+		// WARNING: don't uncomment on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
+	//	bUseUnityBuild = false;
 	}
 }
