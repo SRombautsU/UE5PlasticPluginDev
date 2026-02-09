561586cbee910d9adaf1c54a3d5c83f891b2b853
Sébastien Rombauts <sebastien.rombauts@unity3d.com>
2026-02-09 21:32:27 +0100

    Uncomment for UE 5.7

--- Source/UE5PlasticPluginDevEditor.Target.cs
+++ Source/UE5PlasticPluginDevEditor.Target.cs
@@ -12,13 +12,13 @@ public class UE5PlasticPluginDevEditorTarget : TargetRules
 		ExtraModuleNames.Add("UE5PlasticPluginDev");
 
 		// Required starting from UE 5.1
-		// IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 
 		// Required starting from UE 5.4
-		// WindowsPlatform.bStrictConformanceMode = true;
+		WindowsPlatform.bStrictConformanceMode = true;
 
 		// Required starting from UE 5.5
-		// CppStandard = CppStandardVersion.Cpp20;
+		CppStandard = CppStandardVersion.Cpp20;
 
 		// Set to false to rebuild the whole project without Unity Build, compiling each cpp source file individually, in order to test Includ Whay You Use (IWYU) policy
 		// WARNING: don't disable on UnrealEngine Source Build, else you will trigger a full new Engine build (but easy to revert, will just relink 1200 lib/dll)
